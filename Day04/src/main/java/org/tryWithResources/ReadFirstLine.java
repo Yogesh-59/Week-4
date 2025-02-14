256fe73c962c8bac3860c68e78657619479c0686
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-02-14 13:13:22 +0530

    [Deepraj] add:a Java program that reads the first line of a file named info.txt using BufferedReader. Use try-with-resources to ensure the file is automatically closed after reading. Handle any IOException that may occur.

---
+++ Day04/src/main/java/org/tryWithResources/ReadFirstLine.java
@@ -0,0 +1,18 @@
+package org.tryWithResources;
+import java.io.*;
+public class ReadFirstLine {
+    public static void main(String[] args) {
+        String fileName = "info.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String firstLine = br.readLine();
+            if (firstLine != null) {
+                System.out.println(firstLine);
+            } else {
+                System.out.println("File is empty");
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
