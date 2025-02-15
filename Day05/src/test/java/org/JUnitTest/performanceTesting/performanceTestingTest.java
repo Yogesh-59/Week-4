44fc61bdf380ef5687654e1c3302a9359999896e
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-02-16 03:00:30 +0530

    [Deepraj] add: JUnit test for performanceTesting

---
+++ Day05/src/test/java/org/JUnitTest/performanceTesting/performanceTestingTest.java
@@ -0,0 +1,24 @@
+package org.JUnitTest.performanceTesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Timeout;
+import java.util.concurrent.TimeUnit;
+class performanceTestingTest {
+    public NumberUtils numberUtils = new NumberUtils();
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 7, 9})
+    void testIsEven(int number) {
+        boolean expected = (number % 2 == 0);
+        assertEquals(expected, numberUtils.isEven(number));
+    }
+
+    @Test
+    @Timeout(value = 2, unit = TimeUnit.SECONDS)
+    void testLongRunningTask() throws InterruptedException {
+        numberUtils.longRunningTask();
+    }
+}
