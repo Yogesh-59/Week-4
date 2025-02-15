b278d5e5ef04f88e1212974b0b1ee6e63d91156a
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-02-15 20:21:49 +0530

    [Deepraj] add: A program That performing JUnit test

---
+++ Day05/src/main/java/org/JUnitTest/testingCalculatorClass/TestingCalculatorClass.java
@@ -0,0 +1,22 @@
+package org.JUnitTest.testingCalculatorClass;
+
+public class TestingCalculatorClass {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+   public  int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
