715a6f180e2fcac17abd4936d8b937c52098fe1e
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-02-15 20:27:38 +0530

    [Deepraj] add: A program That testing String Utility Methods

---
+++ Day05/src/main/java/org/JUnitTest/testingStringUtilityMethods/TestUtilityMethod.java
@@ -0,0 +1,16 @@
+package org.JUnitTest.testingStringUtilityMethods;
+
+public class TestUtilityMethod {
+    public String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public boolean isPalindrome(String str) {
+        String reversed = reverse(str);
+        return str.equalsIgnoreCase(reversed);
+    }
+
+    public String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
